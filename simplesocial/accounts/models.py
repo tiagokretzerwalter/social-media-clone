5fc5bce4a45683087f26df928f5f59c6b462d434
Tiago Kretzer Walter <tiagokwalter@gmail.com>
2020-10-23 11:28:30 -0300

    first form, model and view - sign up

--- simplesocial/accounts/models.py
+++ simplesocial/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib import auth
 
 # Create your models here.
+class User(auth.models.User,auth.models.PermissionsMixin):
+# username attribute comes with auth
+    def __str__(self):
+        return "@{}".format(self.username)
+    
--- simplesocial/accounts/views.py
+++ simplesocial/accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverselazy
+from django.views.generic import CreateView
+from . import forms
 
 # Create your views here.
+class SignUp(CreateView):
+    form_class = forms.UserCreateForm
+    success_url = reverselazy('login')
+    template_name = 'accounts/signup.html'
