8fd5117c24aa246376096a0ea8f535469f7c840f
Tiago Kretzer Walter <tiagokwalter@gmail.com>
2020-12-15 16:40:12 -0300

    class based views for posts

--- simplesocial/posts/views.py
+++ simplesocial/posts/views.py
@@ -1,3 +1,80 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+# Someone has to be logged in to do post actions, such as creating
+from django.core.urlresolvers import reverse_lazy
+# in case someone wants to delete a post
+
+
+from django.http import Http404
+from django.views import generic
+
+from braces.views import SelectRelatedMixin
+
+from . import models
+from . import forms
+
+from django.contrib.auth import get_user_model
+# This function allows the following:
+User = get_user_model()
+# This means that when someone is logged in to a session, I am able to use this user object as the current user and call things off of that. 
+
+class PostList(SelectRelatedMixin,generic.ListView):
+    model = models.Post
+    select_related = ('user', 'group')
+
+
+class UserPosts(generic.ListView):
+    model = models.Post
+    template_name = 'posts/user_post_list.html'
+
+    def get_queryset(self):
+        try:
+            self.post.user = User.objects.prefetch_related('posts').get(username__iexact=self.kwargs.get('username'))
+        except User.DoesNotExist:
+            raise Http404
+        else:
+            return self.post_user.posts.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_user"] = self.post_user
+        return context
+    
+class PostDetail(SelectRelatedMixin,generic.DetailView):
+    model = models.Post
+    select_related = ('user','group')
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user__username__iexact=self.kwargs.get('username'))
+
+
+class CreatePost(LoginRequiredMixin,SelectRelatedMixin,generic.CreateView):
+    # the fields someone can edit:
+    fields = ('message','group')
+    model = models.Post
+
+    def form_valid(self,form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        # This is to connect the actual post to the user itself
+        return super().form_valid(form)
+
+class DeletePost(LoginRequiredMixin,SelectRelatedMixin,generic.DeleteView):
+
+    model = models.Post
+    select_related = ('user','group')
+    success_url = reverse_lazy('posts:all')
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user_id = self.request.user.id)
+
+    def delete (self,*args,**kwargs):
+        messages.success(self.request,'Post Delete')
+        return super().delete(*args, **kwargs)
+    
+    
+    
